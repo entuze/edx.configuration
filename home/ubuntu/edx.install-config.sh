89ec658a0764e040ce92899b8808be654d3e931c
Lawrence McDaniel <lpm0073@gmail.com>
2019-04-22 20:10:02 -0500

    add figures to installed_apps

--- home/ubuntu/edx.install-config.sh
+++ home/ubuntu/edx.install-config.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 
-rm -rf /root/edx.configuration
+sudo rm -rf /home/ubuntu/edx.configuration
 git clone https://github.com/entuze/edx.configuration.git
 
-sudo cp /root/edx.configuration/edx/app/edxapp/*.json /edx/app/edxapp/
+# Install application configuration files
+sudo cp /home/ubuntu/edx.configuration/edx/app/edxapp/*.json /edx/app/edxapp/
 sudo chown edxapp /edx/app/edxapp/*.json
 sudo chgrp www-data /edx/app/edxapp/*.json
 
 # Copy our modified nginx configurations for lms and cms
 # these contain lets encrypt ssl certificate and https redirection
-cp /root/edx.configuration/edx/app/nginx/sites-available/lms /edx/app/nginx/sites-available/
-cp /root/edx.configuration/edx/app/nginx/sites-available/cms /edx/app/nginx/sites-available/
+sudo cp /home/ubuntu/edx.configuration/edx/app/nginx/sites-available/lms /edx/app/nginx/sites-available/
+sudo cp /home/ubuntu/edx.configuration/edx/app/nginx/sites-available/cms /edx/app/nginx/sites-available/
 
 # copy server-vars and any other mods to ansible-related work flows
-cp /root/edx.configuration/edx/app/edx_ansible/*.* /edx/app/edx_ansible/
+sudo cp /home/ubuntu/edx.configuration/edx/app/edx_ansible/*.* /edx/app/edx_ansible/
+
+# copy theme files into their run-time location
+sudo rm -r /edx/app/edxapp/edx-platform/themes/entuze-theme
+sudo cp -R /home/ubuntu/edx.configuration/themes/entuze-theme /edx/app/edxapp/edx-platform/themes/
+sudo chown -R edxapp /home/ubuntu/edx.configuration/themes/
+sudo chgrp -R edxapp /home/ubuntu/edx.configuration/themes/
 
 sudo service nginx restart
